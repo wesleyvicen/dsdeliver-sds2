c5d37a00d9c9f4fe9b8fb9cde62d40c732fb40b0
Wesley Vicente <wesley1535@hotmail.com>
2021-01-11 23:17:14 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/sddeliver/SddeliverApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sddeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SddeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SddeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/sddeliver/SddeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sddeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SddeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
